f82324ecec617f3276296183997045ba18b1369e
gatino <gatinocorp@gmail.com>
2023-10-30 14:56:09 -0300

    ...

--- 001 - Menu Hamburger/main.js
+++ 001 - Menu Hamburger/main.js
@@ -5,3 +5,15 @@ btHamburger.addEventListener("click", () => {
   btHamburger.classList.toggle("opened")
   mainMenu.classList.toggle("opened")
 })
+
+// ----------------------------------------------------------------------------------
+
+const btDialog = document.querySelector(".bt-dialog")
+const btModal = document.querySelector(".bt-modal")
+const dialog = document.querySelector("dialog")
+
+btDialog.addEventListener("click", () => dialog.show())
+btModal.addEventListener("click", () => dialog.showModal())
+
+document.querySelectorAll("dialog button").forEach(
+  bt => bt.addEventListener("click", () => dialog.close()))

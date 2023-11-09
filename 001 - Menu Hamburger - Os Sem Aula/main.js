8d5f4a5e8ab512113502a4e673c6b3aeb6f19c4b
gatino <gatinocorp@gmail.com>
2023-11-09 16:14:56 -0300

    ...

--- 001 - Menu Hamburger - Os Sem Aula/main.js
+++ 001 - Menu Hamburger - Os Sem Aula/main.js
@@ -3,3 +3,15 @@ const btToggleMenu = document.querySelector(".menu-hamburger")
 btToggleMenu.addEventListener("click", () => {
   document.body.classList.toggle("open-nav")
 })
+
+const dialog = document.querySelector("dialog")
+const btDialog = document.querySelector(".bt-dialog")
+const btModal = document.querySelector(".bt-modal")
+
+btDialog.addEventListener('click', () => dialog.show())
+btModal.addEventListener('click', () => dialog.showModal())
+
+document.querySelectorAll("dialog button").forEach(
+  bt => bt.addEventListener("click", () => dialog.close()
+  )
+)
